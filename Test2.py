20e278da5415dfbd4d0eb82162c41d9ad368f1d3
Ted Changus <ted@ip-10-2-184-112.us-west-2.compute.internal>
2019-06-05 16:36:33 -0400

    adding args

--- Test2.py
+++ Test2.py
@@ -1,4 +1,12 @@
+import argparse
 import spell.client
+
+p = argparse.ArgumentParser()
+p.add_argument("--photo", default="./demo/imgs/ansel_adams3.jpg", help="Path to input photo (must be comitted)")
+args = p.parse_args()
+
+photo_path = args.photo
+
 client = spell.client.from_environment()
 
 ppackages = ["scikit-image","numpy","matplotlib","scipy"]
@@ -10,7 +18,7 @@ r1.wait_status(client.runs.COMPLETE)
 r2=client.runs.new(github_url="https://github.com/richzhang/colorization",
 pip_packages=ppackages, apt_packages=apackages,attached_resources={"runs/{}/models".format(r1.id):"/mnt/models"},framework="caffe",
 machine_type="K80",
-command="python colorization/colorize.py -img_in ./demo/imgs/ansel_adams3.jpg -img_out ./output.png --caffemodel /mnt/models/colorization_release_v2.caffemodel")
+command="python colorization/colorize.py -img_in {} -img_out ./output.png --caffemodel /mnt/models/colorization_release_v2.caffemodel".format(photo_path))
 r2.wait_status(client.runs.COMPLETE)
 r2.cp(source_path="output.png", destination_directory=None)
 r3=client.run.new(command="cp output.png ./tutorial_outputs")
