2c5859d436cf74f45c986b6e2a9f117924547cc9
Ted Changus <ted@ip-10-2-184-112.us-west-2.compute.internal>
2019-06-05 17:27:02 -0400

    asd

--- Test2.py
+++ Test2.py
@@ -12,11 +12,10 @@ client = spell.client.from_environment()
 ppackages = ["scikit-image","numpy","matplotlib","scipy"]
 apackages = ["python-tk"]
 
-r1=client.runs.new(github_url="https://github.com/richzhang/colorization", command="colorization/models/fetch_release_models.sh")
+r1=client.runs.new(command="colorization/models/fetch_release_models.sh")
 r1.wait_status(client.runs.COMPLETE)
 
-r2=client.runs.new(github_url="https://github.com/richzhang/colorization",
-pip_packages=ppackages, apt_packages=apackages,attached_resources={"runs/{}/models".format(r1.id):"/mnt/models"},framework="caffe",
+r2=client.runs.new(pip_packages=ppackages, apt_packages=apackages,attached_resources={"runs/{}/models".format(r1.id):"/mnt/models"},framework="caffe",
 machine_type="K80",
 command="python colorization/colorize.py -img_in {} -img_out ./output.png --caffemodel /mnt/models/colorization_release_v2.caffemodel".format(photo_path))
 r2.wait_status(client.runs.COMPLETE)
