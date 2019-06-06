d994e18c207494e3867463556801e31c3fb392d7
Ted Changus <ted@ip-10-2-184-112.us-west-2.compute.internal>
2019-06-06 15:06:13 -0400

    Translation

--- Recognition.py
+++ Recognition.py
@@ -8,5 +8,5 @@ r=client.runs.new(machine_type="K80", command="python mnist/main.py", commit_lab
 print("waiting for run {} to finish".format(r.id))
 r.wait_status(client.runs.COMPLETE)
 
-r=client.runs.new(machine_type="K80", command="python vae/main.py",
-attached_resources={"runs/{}/data":"".format(r.id)}, commit_label=LABEL)
+# r=client.runs.new(machine_type="K80", command="python vae/main.py",
+# attached_resources={"runs/{}/data":"".format(r.id)}, commit_label=LABEL)
--- Translation.py
+++ Translation.py
@@ -15,3 +15,8 @@ r3=client.runs.new(attached_resources={"runs/{}/nmt_data".format(r.id):"nmt_data
 commit_label=LABEL,
 command="tail -n10 nmt_data nmt_data/tst2013.vi > my_infer_file.vi && cp -r nmt_model_tmp nmt_model && python -m nmt.nmt  --out_dir=nmt_model --inference_input_file=my_infer_file.vi --inference_output_file=output_infer && cat output_infer")
 r3.wait_status(client.runs.COMPLETE)
+
+r4=client.runs.new(attached_resources={"runs/{}/nmt_data".format(r.id):"nmt_data"},
+commit_label=LABEL,
+command="mkdir nmt_model && export PYTHONIOENCODING=UTF-8 && python -m nmt.nmt  --attention=scaled_luong --src=vi --tgt=en --vocab_prefix=nmt_data/vocab  --train_prefix=nmt_data/train --dev_prefix=nmt_data/tst2012  --test_prefix=nmt_data/tst2013 --out_dir=nmt_attention_model --num_train_steps=12000 --steps_per_stats=100 --num_layers=2 --num_units=128 --dropout=0.2 --metrics=bleu")
+r4.wait_status(client.runs.COMPLETE)
