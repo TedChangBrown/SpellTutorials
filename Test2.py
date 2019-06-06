26331cad99042c929788baaa473c1383d308743e
Ted Changus <ted@ip-10-2-184-112.us-west-2.compute.internal>
2019-06-06 11:07:55 -0400

    asdf

--- Test2.py
+++ Test2.py
@@ -1,8 +1,9 @@
 import argparse
 import spell.client
 
+DEFAULT_PHOTO = "./demo/imgs/ansel_adams3.jpg"
 p = argparse.ArgumentParser()
-p.add_argument("--photo", default="./demo/imgs/ansel_adams3.jpg", help="Path to input photo (must be comitted)")
+p.add_argument("--photo", default=DEFAULT_PHOTO, help="Path to input photo (must be comitted)")
 args = p.parse_args()
 
 photo_path = args.photo
@@ -15,7 +16,12 @@ apackages = ["python-tk"]
 r1=client.runs.new(commit_label="colorizer",command="colorization/models/fetch_release_models.sh")
 r1.wait_status(client.runs.COMPLETE)
 
-r2=client.runs.new(commit_label="colorizer",pip_packages=ppackages, apt_packages=apackages,attached_resources={"runs/{}/models".format(r1.id):"/mnt/models"},framework="caffe",
+if photo_path == DEFAULT_PHOTO:
+    commit_label = "colorizer"
+else:
+    commit_label = "examples"
+
+r2=client.runs.new(commit_label=commit_label,pip_packages=ppackages, apt_packages=apackages,attached_resources={"runs/{}/models".format(r1.id):"/mnt/models"},framework="caffe",
 machine_type="K80",
 command="python colorization/colorize.py -img_in {} -img_out ./output.png --caffemodel /mnt/models/colorization_release_v2.caffemodel".format(photo_path))
 r2.wait_status(client.runs.COMPLETE)
