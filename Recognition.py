cac1dcde6fd84ea4656ddf01c3c6195903d036ed
Ted Changus <ted@ip-10-2-184-112.us-west-2.compute.internal>
2019-06-06 11:35:32 -0400

    asdf

--- Recognition.py
+++ Recognition.py
@@ -7,3 +7,6 @@ LABEL="number-recognition"
 r=client.runs.new(machine_type="K80", command="python mnist/main.py", commit_label=LABEL)
 print("waiting for run {} to finish".format(r.id))
 r.wait_status(client.runs.COMPLETE)
+
+r=client.runs.new(machine_type="K80", command="python vae/main.py",
+attached_resources="runs/{}/data".format(r.id), commit_label=LABEL)
