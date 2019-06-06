90339b10acb2f3667a3ce23de4462ec71a14b871
Ted Changus <ted@ip-10-2-184-112.us-west-2.compute.internal>
2019-06-06 10:49:23 -0400

    asd

--- workflowspelltest.py
+++ workflowspelltest.py
@@ -1,4 +1,4 @@
 import spell.client
 client = spell.client.from_environment()
 #r=client.runs.new(command="ls")
-r=client.runs.new(command="python ./print.py")
+r=client.runs.new(command="python ./print.py", commit_label="main")
