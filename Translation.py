4f77c1ff34e13a1b41bcca1beba503c9f78bd097
Ted Changus <ted@ip-10-2-184-112.us-west-2.compute.internal>
2019-06-06 12:04:29 -0400

    mount

--- Translation.py
+++ Translation.py
@@ -6,7 +6,7 @@ LABEL="translation"
 r=client.runs.new(command="nmt/scripts/download_iwslt15.sh nmt_data",commit_label=LABEL)
 r.wait_status(client.runs.COMPLETE)
 
-r=client.runs.new(attached_resources={"runs/{}/nmt_data":"/nmt_data"},
+r=client.runs.new(attached_resources={"runs/{}/nmt_data".format(r.id):"/nmt_data"},
 commit_label=LABEL, machine_type="K80",
 command="mkdir nmt_model export PYTHONIOENCODING=UTF-8 python -m nmt.nmt  --src=vi --tgt=en --vocab_prefix=nmt_data/vocab  --train_prefix=nmt_data/train --dev_prefix=nmt_data/tst2012  --test_prefix=nmt_data/tst2013 --out_dir=nmt_model --num_train_steps=12000 --steps_per_stats=100 --num_layers=2 --num_units=128 --dropout=0.2 --metrics=bleu")
 r.wait_status(client.runs.COMPLETE)
