afb01654d12ba901722b5821e3b42091a73869c6
Ted Changus <ted@ip-10-2-184-112.us-west-2.compute.internal>
2019-06-06 16:06:37 -0400

    mount removed

--- Recognition.py
+++ Recognition.py
@@ -5,9 +5,10 @@ client = spell.client.from_environment()
 LABEL="number-recognition"
 
 r1=client.runs.new(machine_type="K80", commit_label="main", command="python fetch_data.py")
+
 r2=client.runs.new(machine_type="K80", command="python mnist/main.py", commit_label=LABEL)
 print("waiting for run {} to finish".format(r1.id))
 r2.wait_status(client.runs.COMPLETE)
 
 r=client.runs.new(machine_type="K80", command="python vae/main.py",
-attached_resources={"runs/{}/data".format(r1.id):None}, commit_label=LABEL)
+commit_label=LABEL)
