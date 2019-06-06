6210eb037550baba710e6bd92dc6efb8f06df6e3
Ted Changus <ted@ip-10-2-184-112.us-west-2.compute.internal>
2019-06-06 16:26:57 -0400

    wait

--- Recognition.py
+++ Recognition.py
@@ -5,6 +5,7 @@ client = spell.client.from_environment()
 LABEL="number-recognition"
 
 r1=client.runs.new(machine_type="K80", commit_label="main", command="python fetch_data.py")
+r1.wait_status(client.runs.COMPLETE)
 # r2=client.runs.new(machine_type="K80", command="python mnist/main.py", commit_label=LABEL)
 # print("waiting for run {} to finish".format(r1.id))
 # r2.wait_status(client.runs.COMPLETE)
