56e140c6e4983ffa9bd95f81399f23b2eafb85fa
Ted Changus <ted@ip-10-2-184-112.us-west-2.compute.internal>
2019-06-06 16:29:11 -0400

    testing

--- Recognition.py
+++ Recognition.py
@@ -4,11 +4,11 @@ client = spell.client.from_environment()
 
 LABEL="number-recognition"
 
-r1=client.runs.new(machine_type="K80", commit_label="main", command="python fetch_data.py")
-r1.wait_status(client.runs.COMPLETE)
+# r1=client.runs.new(machine_type="K80", commit_label="main", command="python fetch_data.py")
+# r1.wait_status(client.runs.COMPLETE)
 # r2=client.runs.new(machine_type="K80", command="python mnist/main.py", commit_label=LABEL)
 # print("waiting for run {} to finish".format(r1.id))
 # r2.wait_status(client.runs.COMPLETE)
 
 r=client.runs.new(machine_type="K80", command="python vae/main.py",
-attached_resources={"runs/{}/data".format(r1.id):None}, commit_label=LABEL)
+attached_resources={"runs/291/data", commit_label=LABEL)
