823d910b933b0aea117cb847a62676f2f8a681d8
Ted Changus <ted@ip-10-2-184-112.us-west-2.compute.internal>
2019-06-07 12:17:11 -0400

    asdfsa

--- StyleTransfer.py
+++ StyleTransfer.py
@@ -17,6 +17,12 @@ client=spell.client.from_environment()
 r=client.runs.new(commit_label="StyleTransfer", command="./setup.sh")
 r.wait_status(client.runs.COMPLETE)
 
-r=client.runs.new(attached_resources={"runs/{}/data".format(r.id):"datasets"},framework="tensorflow",
-pip_packages=pip_packages, apt_packages=apt_packages,machine_type="V100",
-command="python style.py --checkpoint-dir ckpt --style {} --style-weight 1.5e2 --train-path datasets/train2014 --vgg-path datasets/imagenet-vgg-verydeep-19.mat".format(args.transfer_style)
+r=client.runs.new(attached_resources={"runs/{}/data".format("341"):"datasets"},framework="tensorflow",
+pip_packages=pip_packages, apt_packages=apt_packages,machine_type="V100",commit_label="StyleTransfer",
+command="python style.py --checkpoint-dir ckpt --style {} --style-weight 1.5e2 --train-path datasets/train2014 --vgg-path datasets/imagenet-vgg-verydeep-19.mat".format(args.transfer_style))
+r.wait_status(client.runs.COMPLETE)
+
+r=client.runs.new(commit_label="StyleTransfer",attached_resources={"runs/{}/ckpt".format(r.id):None},
+machine_type="V100", framework=tensorflow,pip_packages=pip_packages, apt_packages=apt_packages,
+command="python evaluate.py --checkpoint ckpt --in-path images/input/  --out-path images/output/  --allow-different-dimensions")
+r.wait_status(client.runs.COMPLETE)
