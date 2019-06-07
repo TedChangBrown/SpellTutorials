9f98da561536637c1b236e874536bd5d7ff18e8d
Ted Changus <ted@ip-10-2-184-112.us-west-2.compute.internal>
2019-06-07 10:48:21 -0400

    fix

--- workflow.py
+++ workflow.py
@@ -1,17 +1,6 @@
 import argparse
 import spell.client
 
-DEFAULT_COLORIZER_PHOTO = "ansel_adams3.jpg"
-
-p = argparse.ArgumentParser()
-p.add_argument("--photo", default=DEFAULT_COLORIZER_PHOTO, help="Name of input photo (must be comitted to colorization/demo/images)")
-
-args = p.parse_args()
-client=spell.client.from_environment()
-
-colorizer_run(client, "./demo/imgs/"+args.photo)
-
-
 def colorizer_run(client, photo_path):
     ppackages = ["scikit-image","numpy","matplotlib","scipy"]
     apackages = ["python-tk"]
@@ -23,3 +12,14 @@ def colorizer_run(client, photo_path):
     machine_type="K80",
     command="python colorization/colorize.py -img_in {} -img_out ./output.png --caffemodel /mnt/models/colorization_release_v2.caffemodel".format(photo_path))
     r2.wait_status(client.runs.COMPLETE)
+
+
+DEFAULT_COLORIZER_PHOTO = "ansel_adams3.jpg"
+
+p = argparse.ArgumentParser()
+p.add_argument("--photo", default=DEFAULT_COLORIZER_PHOTO, help="Name of input photo (must be comitted to colorization/demo/images)")
+
+args = p.parse_args()
+client=spell.client.from_environment()
+
+colorizer_run(client, "./demo/imgs/"+args.photo)
