2c88dfa9a46ec11dd2cfa56703eb6d27d74ca109
Ted Changus <ted@ip-10-2-184-158.us-west-2.compute.internal>
2019-06-27 14:46:55 -0400

    asda

--- metrictest.py
+++ metrictest.py
@@ -8,7 +8,13 @@ if __name__ == "__main__":
   num = 0
   step = 0
   for i in range(30):
-    print("Step " + str(step) + ": " + str(i))
-    metrics.send_metric("walk", i)
+    add = (random.random() - 0.5) * float(sys.argv[1])
+    num += add
+    positive += add
+    positive = max(0, positive)
+    print("Step " + str(step) + ": " + str(add) + " - num: " + str(num) + " - pos: " + str(positive))
+    metrics.send_metric("pos_walk", positive)
+    metrics.send_metric("walk", num)
+    metrics.send_metric("text", "Hi! Number is " + str(num))
     step += 1
     time.sleep(1)
